555ee0c76b98bb85bcba081c94930770fc214eb2
guilhermeribeirovieira-ops <guilherme.ribeiro.vieira@escola.pr.gov.br>
2026-08-06 11:46:12 +0000

    v3

---
+++ script.js
@@ -0,0 +1,4 @@
+const postsContainer = document.getElementById("posts-container");
+const inputBusca = document.getElementById("input-busca");
+const commentForm = document.getElementById("comment-form");
+const commentsList = document.getElementById("comments-list");
